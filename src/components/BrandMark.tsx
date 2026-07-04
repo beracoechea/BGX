f514b032112764a408b9144245b6c0bfaad0ac31
beracoechea <jofraber203@gmail.com>
2026-07-03 23:46:02 -0600

    Ajuste en la X

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -8,8 +8,8 @@ interface BrandMarkProps {
 export function BrandMark({ className = '', size = 'md' }: BrandMarkProps) {
   return (
     <span className={`brand-x brand-x--${size} ${className}`} aria-hidden="true">
-      <span className="brand-x__arm brand-x__arm--bl" />
-      <span className="brand-x__arm brand-x__arm--tr" />
+      <span className="brand-x__arm brand-x__arm--1" />
+      <span className="brand-x__arm brand-x__arm--2" />
       <span className="brand-x__pixel brand-x__pixel--1" />
       <span className="brand-x__pixel brand-x__pixel--2" />
       <span className="brand-x__pixel brand-x__pixel--3" />
